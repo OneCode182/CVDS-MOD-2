a71524ec995757a3c9219771af3dd74206c25d36
unknown <juan.lcardenas@labinfo.is.escuelaing.edu.co>
2025-04-30 08:04:19 -0500

    BRANCH: feature tests

---
+++ src/test/java/eci/cvds/mod2/modules/ReservationTest.java
@@ -0,0 +1,21 @@
+package eci.cvds.mod2.modules;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addLoan() {
+    }
+
+    @Test
+    void getLoans() {
+    }
+}
